edf4038413a47ba92c00dd2c2ba9f1ec3f12ea13
mste33 <93059695+mste33@users.noreply.github.com>
2025-05-15 00:52:40 -0400

    Delete Pets Website directory

--- Pets Website/js/datetime.js
+++
@@ -1,9 +0,0 @@
-function updateDateTime() {
-    const now = new Date();
-    const dateStr = now.toLocaleDateString();
-    const timeStr = now.toLocaleTimeString();
-    document.getElementById('datetime').textContent = dateStr + ' | ' + timeStr;
-}
-
-updateDateTime();
-setInterval(updateDateTime, 1000); 
